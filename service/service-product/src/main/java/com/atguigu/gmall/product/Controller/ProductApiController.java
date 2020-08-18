32fac82426d7233401b68ff8c08989c9a8477b52
hsc <hsc862442629@163.com>
2020-08-18 21:10:38 +0800

    测试Controller成功

--- service/service-product/src/main/java/com/atguigu/gmall/product/Controller/ProductApiController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/Controller/ProductApiController.java
@@ -1,7 +1,18 @@
 package com.atguigu.gmall.product.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author ccc
  * @create 2020-08-18 20:30
  */
+@RestController
+@RequestMapping("admin/product")
 public class ProductApiController {
+
+    @RequestMapping("testApiController")
+    public String testApiController(){
+        return "testApiController";
+    }
 }
