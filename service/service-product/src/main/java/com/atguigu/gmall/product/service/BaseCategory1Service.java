21e9bff6121f6d8295d0135f8011e695159defc3
hsc <hsc862442629@163.com>
2020-08-19 08:34:13 +0800

    dev 加入统一返回结果集2

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/BaseCategory1Service.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.product.service;
+
+import com.atguigu.gmall.model.product.BaseCategory1;
+
+import java.util.List;
+
+/**
+ * @author ccc
+ * @create 2020-08-18 21:24
+ */
+public interface BaseCategory1Service {
+    List<BaseCategory1> getCategory1();
+}
