8d398df953fdc034512bee3127424dcf93f376d0
hsc <hsc862442629@163.com>
2020-08-19 08:30:40 +0800

    dev 加入统一返回结果集

--- service/service-product/src/main/java/com/atguigu/gmall/product/Controller/ProductApiController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/Controller/ProductApiController.java
@@ -1,7 +1,12 @@
 package com.atguigu.gmall.product.Controller;
 
+import com.atguigu.gmall.model.product.BaseCategory1;
+import com.atguigu.gmall.product.service.BaseCategory1Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author ccc
@@ -11,8 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("admin/product")
 public class ProductApiController {
 
+    @Autowired
+    BaseCategory1Service baseCategory1Service;
+
     @RequestMapping("testApiController")
-    public String testApiController(){
+    public String testApiController() {
         return "testApiController";
+    }
+
+    //http://api.gmall.com/admin/product/getCategory1
+    @RequestMapping("getCategory1")
+    public List<BaseCategory1> getCategory1() {
+        List<BaseCategory1> baseCategory1List = baseCategory1Service.getCategory1();
+        return baseCategory1List;
     }
 }
