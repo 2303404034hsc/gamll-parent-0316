cb0001b51d36cd1fe733d79505509a290678fead
hsc <hsc862442629@163.com>
2020-08-19 16:05:30 +0800

    mydev 平台属性添加，修改，查询已完成，刚漏提交AttrValueMapper

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/mapper/AttrValueMapper.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.product.mapper;
+
+import com.atguigu.gmall.model.product.BaseAttrValue;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author ccc
+ * @create 2020-08-19 11:35
+ */
+@Mapper
+public interface AttrValueMapper extends BaseMapper<BaseAttrValue> {
+}
