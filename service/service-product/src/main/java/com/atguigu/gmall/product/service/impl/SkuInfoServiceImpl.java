c015aff770dce3cffdf4fd033c152cd1e27d1929
hsc <hsc862442629@163.com>
2020-08-25 10:53:54 +0800

    ccc --- 商品详情 查询skuInfo的时候加入了缓存，和处理缓存的三个问题         缓存穿透 ：通过一个不存在的key 绕过缓存多次访问 db 的过程         缓存击穿 : 某一个热点key,在高频访问过程中突然失效，会导致db的访问量突然暴增         缓存雪崩 : 缓存中 key 使用了相同的过期时间，同时过期，导致db崩溃

--- service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/SkuInfoServiceImpl.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.atguigu.gmall.product.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.gmall.model.product.*;
 import com.atguigu.gmall.product.mapper.SkuAttrValueMapper;
 import com.atguigu.gmall.product.mapper.SkuImageMapper;
@@ -9,13 +10,18 @@ import com.atguigu.gmall.product.service.SkuInfoService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import springfox.documentation.spring.web.json.Json;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author ccc
@@ -35,6 +41,9 @@ public class SkuInfoServiceImpl implements SkuInfoService {
     @Autowired
     SkuImageMapper skuImageMapper;
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
 
     @Override
     public void saveSkuInfo(SkuInfo skuInfo) {
@@ -45,7 +54,7 @@ public class SkuInfoServiceImpl implements SkuInfoService {
         Long spuId = skuInfo.getSpuId();
         //sku_attr_value的保存
         List<SkuAttrValue> skuAttrValueList = skuInfo.getSkuAttrValueList();
-        if(null != skuAttrValueList){
+        if (null != skuAttrValueList) {
             for (SkuAttrValue skuAttrValue : skuAttrValueList) {
                 skuAttrValue.setSkuId(skuId);
                 skuAttrValueMapper.insert(skuAttrValue);
@@ -54,7 +63,7 @@ public class SkuInfoServiceImpl implements SkuInfoService {
 
         //sku_sale_attr_value的保存
         List<SkuSaleAttrValue> skuSaleAttrValueList = skuInfo.getSkuSaleAttrValueList();
-        if(null != skuAttrValueList){
+        if (null != skuAttrValueList) {
             for (SkuSaleAttrValue skuSaleAttrValue : skuSaleAttrValueList) {
                 skuSaleAttrValue.setSkuId(skuId);
                 skuSaleAttrValue.setSpuId(spuId);
@@ -64,7 +73,7 @@ public class SkuInfoServiceImpl implements SkuInfoService {
 
         //sku_image的保存
         List<SkuImage> skuImageList = skuInfo.getSkuImageList();
-        if(null != skuImageList){
+        if (null != skuImageList) {
             for (SkuImage skuImage : skuImageList) {
                 skuImage.setSkuId(skuId);
                 skuImageMapper.insert(skuImage);
@@ -75,14 +84,14 @@ public class SkuInfoServiceImpl implements SkuInfoService {
 
     @Override
     public IPage<SkuInfo> getSkuInfoListByPage(Page<SkuInfo> pageParam) {
-        IPage<SkuInfo> skuInfoIPage = skuInfoMapper.selectPage(pageParam,null);
+        IPage<SkuInfo> skuInfoIPage = skuInfoMapper.selectPage(pageParam, null);
         return skuInfoIPage;
     }
 
     @Override
     public void onSale(String skuId) {
         QueryWrapper<SkuInfo> skuInfoQueryWrapper = new QueryWrapper<>();
-        skuInfoQueryWrapper.eq("id",skuId);
+        skuInfoQueryWrapper.eq("id", skuId);
         SkuInfo skuInfo = skuInfoMapper.selectOne(skuInfoQueryWrapper);
         skuInfo.setIsSale(1);
         skuInfoMapper.updateById(skuInfo);
@@ -91,19 +100,73 @@ public class SkuInfoServiceImpl implements SkuInfoService {
     @Override
     public void cancelSale(String skuId) {
         QueryWrapper<SkuInfo> skuInfoQueryWrapper = new QueryWrapper<>();
-        skuInfoQueryWrapper.eq("id",skuId);
+        skuInfoQueryWrapper.eq("id", skuId);
         SkuInfo skuInfo = skuInfoMapper.selectOne(skuInfoQueryWrapper);
         skuInfo.setIsSale(0);
         skuInfoMapper.updateById(skuInfo);
     }
 
+    //获取商品详情
     @Override
     public SkuInfo getSkuInfo(String skuId) {
+
+        System.out.println(Thread.currentThread().getName() + "申请详情");
+
+        SkuInfo skuInfo = null;
+
+        //查询缓存
+        String skuStrFromCache = (String) redisTemplate.opsForValue().get("sku:" + skuId + ":info");
+
+        if(StringUtils.isBlank(skuStrFromCache)){
+            //分布式缓存锁
+            //
+            Boolean lock = redisTemplate.opsForValue().setIfAbsent("sku:" + skuId + ":lock", 1, 10, TimeUnit.SECONDS);
+            if(lock){
+                //查询db
+                skuInfo = getSkuInfoFromDb(skuId);
+
+                System.out.println(Thread.currentThread().getName() + "拿到分布式锁");
+
+                //同步缓存
+                if(null != skuInfo){
+                    redisTemplate.opsForValue().set("sku:" + skuId + ":info", JSON.toJSONString(skuInfo));
+                }else{
+                    //在缓存添加一个空值 10秒后过期
+                    redisTemplate.opsForValue().set("sku:" + skuId + ":info", JSON.toJSONString(new SkuInfo()),10,TimeUnit.SECONDS);
+                }
+//                try {
+//                    Thread.sleep(5000);
+//                }catch (Exception e){
+//                    e.printStackTrace();
+//                }
+                //自己线程操作完毕 归还分布式锁
+                redisTemplate.delete("sku:" + skuId + ":lock");
+
+                System.out.println(Thread.currentThread().getName() + "归还分布式锁");
+            }else{
+                // 自旋
+                System.out.println(Thread.currentThread().getName() + "没有获得分布式锁，开始自旋");
+                try {
+                    Thread.sleep(3000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                return  getSkuInfo(skuId);
+            }
+        }else{
+            //缓存中有，直接解析
+            skuInfo = JSON.parseObject(skuStrFromCache,SkuInfo.class);
+        }
+
+        return skuInfo;
+    }
+
+    private SkuInfo getSkuInfoFromDb(String skuId) {
         //查询基本表信息
         SkuInfo skuInfo = skuInfoMapper.selectById(skuId);
         //封装图片信息
         QueryWrapper<SkuImage> skuImageWrapper = new QueryWrapper<>();
-        skuImageWrapper.eq("sku_id",skuId);
+        skuImageWrapper.eq("sku_id", skuId);
         List<SkuImage> skuImages = skuImageMapper.selectList(skuImageWrapper);
         skuInfo.setSkuImageList(skuImages);
         return skuInfo;
@@ -117,12 +180,12 @@ public class SkuInfoServiceImpl implements SkuInfoService {
 
     @Override
     public Map<String, String> getSkuValueIdsMap(Long spuId) {
-        List<Map<String,Object>> resultMapList =  skuSaleAttrValueMapper.selectSkuValueIdsMap(spuId);
-        Map<String,String> valueIdsMap = new HashMap<>();
+        List<Map<String, Object>> resultMapList = skuSaleAttrValueMapper.selectSkuValueIdsMap(spuId);
+        Map<String, String> valueIdsMap = new HashMap<>();
         for (Map<String, Object> map : resultMapList) {
             String skuId = map.get("sku_id").toString();
             String valueId = map.get("value_ids").toString();
-            valueIdsMap.put(valueId,skuId);
+            valueIdsMap.put(valueId, skuId);
         }
         return valueIdsMap;
     }
