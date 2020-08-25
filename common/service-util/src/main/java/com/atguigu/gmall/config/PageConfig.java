7e326d5f76a257e1c2d525992b254d3f28d104c5
hsc <hsc862442629@163.com>
2020-08-25 14:21:05 +0800

    ccc --- 加入了分页插件的配置 在common里面的service-util里面加入了配置类

---
+++ common/service-util/src/main/java/com/atguigu/gmall/config/PageConfig.java
@@ -0,0 +1,20 @@
+package com.atguigu.gmall.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ccc
+ * @create 2020-08-25 14:15
+ */
+@Configuration
+public class PageConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
