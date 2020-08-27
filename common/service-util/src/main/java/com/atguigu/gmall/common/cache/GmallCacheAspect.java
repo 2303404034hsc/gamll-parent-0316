20d9f1d0cc6a4d1ad56f4396a7f19487779837d6
hsc <hsc862442629@163.com>
2020-08-27 14:04:58 +0800

    ccc --- 商品详情的redisson解决分布式锁

--- common/service-util/src/main/java/com/atguigu/gmall/common/cache/GmallCacheAspect.java
+++ common/service-util/src/main/java/com/atguigu/gmall/common/cache/GmallCacheAspect.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
@@ -27,9 +29,19 @@ public class GmallCacheAspect {
     @Autowired
     RedisTemplate redisTemplate;
 
+    @Autowired
+    RedissonClient redissonClient;
+
     @Around("@annotation(com.atguigu.gmall.common.cache.GmallCache)")
     public Object cacheAroundAdvice(ProceedingJoinPoint point) {
 
+//        return getObject(point);
+        return getObject2(point);
+
+//        return getObject(point);
+    }
+
+    private Object getObject(ProceedingJoinPoint point) {
         Object result;
 
         Integer count = 10;
@@ -96,6 +108,65 @@ public class GmallCacheAspect {
                 //在缓存中自旋
                 return cacheHit(signature, key, count, false);
                 //return cacheHit(signature, key, threadLocal,false);
+            }
+        }
+        //后置通知
+        return result;
+    }
+
+    private Object getObject2(ProceedingJoinPoint point) {
+        Object result;
+
+        Integer count = 10;
+
+        System.out.println("执行被代理之前");
+        //获得参数
+        Object[] args = point.getArgs();
+
+        //
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        GmallCache annotation = signature.getMethod().getAnnotation(GmallCache.class);
+        String key = annotation.prefix() + ":" + args[0];
+
+        //查询缓存
+        result = cacheHit(signature, key, count, true);
+        //result = cacheHit(signature, key, threadLocal, true);
+        //判断有没有值
+        if (null == result) {
+            //获取分布式锁
+            //分布式缓存锁
+            //redis里面的 setnx 只有当键不存在时才会返回成功
+            RLock redissonClientLock = redissonClient.getLock(key);
+            try {
+                boolean lock = redissonClientLock.tryLock(100, 10, TimeUnit.SECONDS);
+                if (lock) {
+                    try {
+                        //访问db
+                        result = point.proceed();
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
+                    //同步到缓存
+                    if (null != result) {
+                        redisTemplate.opsForValue().set(key + ":info", JSON.toJSONString(result));
+                    } else {
+                        //在缓存添加一个空值 10秒后过期
+                        Object o = null;
+                        try {
+                            o = signature.getReturnType().newInstance();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        redisTemplate.opsForValue().set(key + ":info", JSON.toJSONString(o), 10, TimeUnit.SECONDS);
+                    }
+                } else {
+                    //在缓存中自旋
+                    return cacheHit(signature, key, count, false);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                redissonClientLock.unlock();
             }
         }
         //后置通知
